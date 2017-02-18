f5e6cba1e73026704cba9b697309805fc59da810
Anto Sarkissian <antosarkissian1@gmail.com>
2017-02-18 09:26:11 -0500

    first commit

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 var grocery = [
-{product: "cherries", price: 3},
-{product:"cereal", price: 5.25},
-{product: "milk", price: 3.99},
+{product: "Cherries", price: 3.00},
+{product:"Cereal", price: 5.25},
+{product: "Milk", price: 3.99},
 {product: "Butter", price: 4.25},
 {product: "Beer", price: 10.10}];
 
@@ -16,3 +16,15 @@ for (x=0; x<grocery.length; x++){
 }
 
 console.log("total:" +sum);
+
+for (i=0; i<grocery.length; i++){
+  var newDiv = document.getElementById('list');
+  newDiv.innerHTML += "<p>"+grocery[i].product + ":$" + grocery[i].price.toFixed(2)+"</p>";
+};
+
+var sum2 = 0;
+for (x=0; x<grocery.length; x++){
+    var total2 = document.getElementById('total');
+  sum2 += grocery[x].price;
+  total2.innerHTML = "Total $" +sum2.toFixed(2);
+}
